865a2aa9251e35e14dfc652dcc40bfaaad324fa5
wygao8 <gaowenyang@westlake.edu.cn>
2023-10-30 16:28:48 +0000

    Train DELLA on yelp with 8 GPUs.

--- train.sh
+++ train.sh
@@ -1,11 +1,26 @@
-export CUDA_VISIBLE_DEVICES=0,1
-$ThisDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+set -x
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export PYTHONPATH=/gaowenyang/DELLA:$PYTHONPATH
+cd /gaowenyang/DELLA
+#$ThisDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+##Conditional Generation
+# python -u main.py \
+#     --train_file ./data/yelp/yelp.train.txt \
+#     --valid_file ./data/yelp/yelp.valid.txt \
+#     --dataset_type wp \
+#     --per_gpu_train_batch_size 64 \
+#     --pretrained_model ../models/gpt2 \
+#     --model_name della \
+#     --cycle_annealing \
+#    --n_gpus 2 \
+
+#Unconditional Generation
 python -u main.py \
-    --train_file $ThisDir/data/yelp/yelp.train.txt \
-    --valid_file $ThisDir/data/yelp/yelp.valid.txt \
-    --dataset_type wp \
-    --per_gpu_train_batch_size 128 \
+    --train_file ./data/yelp/yelp.train.txt \
+    --valid_file ./data/yelp/yelp.valid.txt \
+    --per_gpu_train_batch_size 16 \
     --model_name della \
+    --pretrained_model ../models/gpt2 \
     --cycle_annealing \
-    --n_gpus 2 \
+    |& tee log.txt
